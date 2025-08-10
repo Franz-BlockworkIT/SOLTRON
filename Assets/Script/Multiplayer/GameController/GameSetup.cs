cf19238930d54e4628847e2a2e7328e4c073e36e
Hafiz Saad Khawar <45258224+Sksaad@users.noreply.github.com>
2025-08-10 13:19:32 +0100

    GameSetup.cs

--- Assets/Script/Multiplayer/GameController/GameSetup.cs
+++ Assets/Script/Multiplayer/GameController/GameSetup.cs
@@ -1,3 +1,7 @@
+// GameSetup.cs
+// Author: Hafiz Saad Khawar
+// Created: 2024-02-04
+// Description: Singleton class that holds spawn points for player instantiation in the Soltron game.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
